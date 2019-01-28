5406bffcdb01b60bdd332fa23d5c7597a917bf44
Pete Johns <pete.johns@envato.com>
2019-01-28 16:59:46 +1100

    3.7 by default, check against 3.8 dev

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Software Development",
